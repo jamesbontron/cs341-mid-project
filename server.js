7adf7b369321470a5516aef66aae6ae260a91a31
Alejandro Naranjo <alejandronaranjo@hotmail.es>
2022-06-03 21:29:01 -0500

    Implement Oatuh with Google+

--- server.js
+++ server.js
@@ -4,6 +4,20 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 8080;
 const connect = require('./db/connect');
+const passport = require('passport');
+const session = require('express-session');
+
+//Passport Config
+require('./config/passport')(passport)
+
+app
+  .use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
+  }))
+  .use(passport.initialize())
+  .use(passport.session());
 
 app
   .use(bodyParser.json())
