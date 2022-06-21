8e62ddb2f28ab47926fd5e271f823c2035b15659
Alejandro Naranjo <alejandronaranjo@hotmail.es>
2022-06-20 22:13:27 -0500

    Change routes

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const routes = require('express').Router();
 
 routes.use('/', require('./swagger'));
 routes.use('/toDoList', require('./toDoList'));
+routes.use('/auth', require('./routes/auth'));
 
 const { ensureAuth, ensureGuest } = require('../middleware/auth')
 
