0f401a8d1616f367553f98559b6defeb96ba694b
Alejandro Naranjo <alejandronaranjo@hotmail.es>
2022-06-20 19:53:52 -0500

    Change passport and routes

--- server.js
+++ server.js
@@ -1,23 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const dotenv = require('dotenv');
 const app = express();
 const port = process.env.PORT || 8080;
 const connect = require('./db/connect');
 const passport = require('passport');
 const session = require('express-session');
 
-//Passport Config
+dotenv.config({ path: '/.env'})
+
 require('./config/passport')(passport)
 
-app
-  .use(session({
-    secret: 'keyboard cat',
-    resave: false,
-    saveUninitialized: false
-  }))
-  .use(passport.initialize())
-  .use(passport.session());
+//Sessions
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized : false
+}))
+
+// Passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
 
 app
   .use(bodyParser.json())
@@ -25,7 +28,8 @@ app
     res.setHeader('Access-Control-Allow-Origin', '*');
     next();
   })
-  .use('/', require('./routes'));
+  .use('/', require('./routes'))
+  .use('/auth', require('./routes/auth'));
 
 connect.initDatabase();
 app.listen(port, () => {
