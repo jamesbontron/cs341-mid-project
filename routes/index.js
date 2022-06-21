38f4fe852e795ec048e5569c32de103ef64e4afd
Alejandro Naranjo <alejandronaranjo@hotmail.es>
2022-06-21 10:17:52 -0500

    fix story routes

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,6 @@ routes.use('/auth', require('./auth'));
 
 const { ensureAuth, ensureGuest } = require('../middleware/auth')
 
-const Story = require('../models/Story')
-
 // @desc    Login/Landing page
 // @route   GET /
 router.get('/', ensureGuest, (req, res) => {
@@ -20,10 +18,8 @@ router.get('/', ensureGuest, (req, res) => {
 // @route   GET /dashboard
 router.get('/dashboard', ensureAuth, async (req, res) => {
   try {
-    const stories = await Story.find({ user: req.user.id }).lean()
     res.render('dashboard', {
-      name: req.user.firstName,
-      stories,
+      name: req.user.firstName
     })
   } catch (err) {
     console.error(err)
