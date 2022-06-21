b31f9d9570aeb62738d0d83e29a0fbcf8987bc75
Alejandro Naranjo <alejandronaranjo@hotmail.es>
2022-06-20 19:53:34 -0500

    Passport functions added

--- routes/index.js
+++ routes/index.js
@@ -2,5 +2,6 @@ const routes = require('express').Router();
 
 routes.use('/', require('./swagger'));
 routes.use('/toDoList', require('./toDoList'));
+routes.use('/home', require('./toDoList'));
 
 module.exports = routes;
